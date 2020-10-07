48ed43068eb5a7ad4b3633588f2012f551e60480
Matheus Alvim <60630664+Alvimatheus@users.noreply.github.com>
2020-10-07 09:44:52 -0300

    Project Setup
    Basic requirements to start a project using React Hooks, Styled Components and Ant Design

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import 'antd/dist/antd.css'
+import '../styles/vars.css'
+import '../styles/global.css'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
